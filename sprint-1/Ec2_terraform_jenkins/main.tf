5edbd81ce84516452291acbaa80b684c7bc9be49
srivathsavanurag <98967875+srivathsavanurag@users.noreply.github.com>
2024-03-14 11:10:07 +0530

    Update main.tf

--- sprint-1/Ec2_terraform_jenkins/main.tf
+++ sprint-1/Ec2_terraform_jenkins/main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
    region     = "us-east-1"
-   access_key = "AKIA5UIDRHDXKBEGCKP3"
-   secret_key = "RRvi4f2B8DQivnLA0ZBnQ8t8TdCJjHXqHReeVz/l"
+   access_key = 
+   secret_key = 
 }
 
 resource "aws_instance" "example" {
